686faaeee3e76c80f1faf885d56bf5b05f175f85
Matheus Hernandes <matheus@hokocloud.com>
2022-03-03 09:58:40 -0300

    Starting base.css

--- src/App.vue
+++ src/App.vue
@@ -28,5 +28,18 @@ export default {
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.flag {
+  width: 30px;
+  height: 30px;
+  border-radius: 50px;
+}
 
 </style>
