1d028c47a123c8e90d6bf911149e4090889a89fb
fabio.f <fabio.f@tn3.com.br>
2022-03-03 10:36:16 -0300

    included vuex and auth flow using store

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/er-home.vue";
 import Login from "../views/er-login.vue";
 
+import store from "../store";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -24,6 +26,9 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // inicio: techo de autenticação, mudar para localadequado
+    store.dispatch('authenticate', true).then(result => console.log(result));
+    // fim: techo de autenticação, mudar para localadequado
     if (localStorage.getItem("userToken") == null) {
       next({
         path: "/login",
