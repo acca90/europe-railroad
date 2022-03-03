5397d7e89bb6bbb8a1d269932294991fce88b80e
Matheus Hernandes <matheus@hokocloud.com>
2022-03-03 18:31:57 -0300

    Updatings support

--- src/views/er-support.vue
+++ src/views/er-support.vue
@@ -7,52 +7,59 @@
     <form class="row form was-validated">
       <div class="col-md-3"></div>
       <div class="col-md-6">
+
+        <div v-if="error" class="alert" :class="alert_class" role="alert">
+          <i class="fa fa-warning"></i>
+          {{ error }}
+        </div>
+
         <div>
-          <label for="emailForm" class="form-label">{{ txt.email }}</label>
+          <label for="emailForm" class="form-label">{{ txt.email }} / Email</label>
           <input
-            required
-            type="email"
-            id="emailForm"
-            class="form-control"
-            v-model="form.email"
-            placeholder="name@example.com"
+              required
+              type="email"
+              id="emailForm"
+              class="form-control"
+              v-model="form.email"
+              placeholder="name@example.com"
           />
         </div>
 
         <div>
-          <label class="form-label">{{ txt.name }}</label>
+          <label class="form-label">{{ txt.name }} / Full Name</label>
           <input
+              required
+              type="text"
+              class="form-control"
+              v-model="form.name"
+          />
+        </div>
+
+        <label class="form-label">{{ txt.subject }} / Subject</label>
+        <input
             required
             type="text"
             class="form-control"
-            v-model="form.name"
-          />
-        </div>
-
-        <label class="form-label">{{ txt.subject }}</label>
-        <input
-          required
-          type="text"
-          class="form-control"
-          v-model="form.subject"
+            v-model="form.subject"
         />
 
-        <label class="form-label">{{ txt.description }}</label>
+        <label class="form-label">{{ txt.description }} / Description</label>
         <!--text area-->
         <textarea
-          class="form-control"
-          aria-label="With textarea"
-          v-model="form.description"
-          rows="5"
+            required
+            class="form-control"
+            aria-label="With textarea"
+            v-model="form.description"
+            rows="5"
         >
         </textarea>
 
         <div id="liveAlertPlaceholder"></div>
         <button
-          style="margin-top: 10px"
-          type="submit"
-          class="btn btn-primary"
-          v-on:click.prevent="validate"
+            style="margin-top: 10px"
+            type="submit"
+            class="btn btn-primary"
+            v-on:click.prevent="validate"
         >
           {{ txt.submit }}
         </button>
@@ -69,6 +76,8 @@ export default {
   },
   data() {
     return {
+      error: '',
+      alert_class: '',
       txt: {
         email: "електронна пошта",
         name: "ім'я",
@@ -90,41 +99,33 @@ export default {
   },
   methods: {
     alert(message, type) {
-      let alertPlaceholder = document.getElementById("liveAlertPlaceholder");
-      let wrapper = document.createElement("div");
-      wrapper.innerHTML =
-        '<div class="alert alert-' +
-        type +
-        ' alert-dismissible" role="alert">' +
-        message +
-        '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
-
-      alertPlaceholder.append(wrapper);
+      this.alert_class = type;
+      this.error = message;
     },
     sendForm() {
       const successOP = (resp) => {
-        this.alert(this.txt.postSuccess, "success");
+        this.alert(this.txt.postSuccess, "alert-success");
       };
       const failOP = (resp) => {
-        this.alert(this.txt.postError, "danger");
+        this.alert(this.txt.postError, "alert-danger");
       };
       this.$axios
-        .post(
-          "https://sc-ukraine.baba.ndmglobal.com/api/execute/email-support",
-          this.form
-        )
-        .then(successOP, failOP);
+          .post(
+              "https://sc-ukraine.baba.ndmglobal.com/api/execute/email-support",
+              this.form
+          )
+          .then(successOP, failOP);
     },
     validate() {
       if (
-        this.form.email ||
-        this.form.name ||
-        this.form.description ||
-        this.form.subject
+          this.form.email ||
+          this.form.name ||
+          this.form.description ||
+          this.form.subject
       ) {
         this.sendForm();
       } else {
-        this.alert(this.txt.formAlert, "alert");
+        this.alert(this.txt.formAlert, "alert-warning");
       }
     },
   },
